5b7f53a8e5dbb62e45976bb70bdaa2e3ea35c7bf
Fulvio Favilla <269078138+fulviofavilla@users.noreply.github.com>
2026-04-28 20:40:25 -0300

    chore: configure vitesse-dark/vitesse-light as code syntax themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,13 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://fulvio.sh',
   integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'vitesse-light',
+        dark: 'vitesse-dark',
+      },
+      defaultColor: false,
+    },
+  },
 });
